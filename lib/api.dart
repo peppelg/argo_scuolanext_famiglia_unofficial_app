f4e2a13ae3db873cee17db23649521b13ffb1e2a
MatteoGalletta <matteo.galletta@hotmail.it>
2021-09-09 15:54:49 +0200

    Aggiunta Schermata Lista Professori

--- lib/api.dart
+++ lib/api.dart
@@ -461,6 +461,20 @@ Future votiscruitinio({var response}) async {
   return scrutinio;
 }
 
+Future listaProfessori({var response}) async {
+  if (response == null) {
+    response = await argoRequest(fullHeaders, 'docenticlasse',
+        {'page': '1', 'start': '0', 'limit': '25'});
+  }
+
+  if (!(response is List) && response.containsKey('error')) {
+    Fluttertoast.showToast(msg: 'Errore sconosciuto:\n\n' + response['error']);
+    return [];
+  }
+
+  return response;
+}
+
 bacheca_parse(elemento) {
   return {
     'oggetto': elemento['desOggetto'],
